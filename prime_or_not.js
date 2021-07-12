21cd9281ba6748bebadb8438a1656c3f568f8ab8
enumeler <enumeler@outlook.com>
2021-07-12 17:20:59 +0545

    Added a JS program to check if prime or not

--- prime_or_not.js
+++ prime_or_not.js
@@ -4,7 +4,7 @@ function prime_or_not(num)
   if(!(num % 2 == 0) && num > 2) /* If a number is even and greater than 2, it is automatically composite */
     {
       half_num = Math.floor(num / 2); /* If we have a number say 100, the biggest number we can hope that divides 100 is 50. 51 or greater number cannot divide 100 */
-      for(i = 3; i < half_num; i+= 2) /* Since half_num is calculated once and never changed, why recalculate it in each loop? /*
+      for(i = 3; i <= half_num; i+= 2) /* Since half_num is calculated once and never changed, why recalculate it in each loop? /*
         {                             /* Also because an even number never divides prime number, we can skip even numbers from check */
           if(num % i == 0)
             {
